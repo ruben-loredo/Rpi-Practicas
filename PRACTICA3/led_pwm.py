47105100fa9aeed75c03b39101b63c8c6cd5dbd4
Rubén Loredo Amaro <ruben_loredo@hotmail.com>
2019-11-09 23:29:59 -0600

    Create led_pwm.py

---
+++ PRACTICA3/led_pwm.py
@@ -0,0 +1,20 @@
+import time
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BCM)
+led=18
+#configura al pin gpio18 como salida
+GPIO.setup(led, GPIO.OUT)   
+p = GPIO.PWM(led, 50)  
+p.start(0)
+try:
+    while 1:
+        for dc in range(0, 101, 5):
+            p.ChangeDutyCycle(dc)
+            time.sleep(0.1)
+        for dc in range(100, -1, -5):
+            p.ChangeDutyCycle(dc)
+            time.sleep(0.1)
+except KeyboardInterrupt:
+    pass
+p.stop()
+GPIO.cleanup()
